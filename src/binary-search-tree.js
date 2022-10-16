8d4ac1c6ee8b00c3216ab44d0901d1bcb09c489a
Vladislav Chervyakov <chervyakovve@mail.ru>
2022-10-16 22:05:00 +0700

    fix find&has methods

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -17,86 +17,92 @@ class BinarySearchTree {
   }
 
   add(data) {
-    this.rootTree = this.addNode(this.rootTree, data)
-  }
+    this.rootTree = addNode(this.rootTree, data)
 
-  addNode(node, data) {
-    if (!node) return new Node(data)
-    if (node.data == data) return data
-
-    if (data < node.data) {
-      node.left = this.addNode(node.left, data)
-
-    } else {
-      node.right = this.addNode(node.right, data)
+    function addNode(node, data) {
+      if (!node) return new Node(data)
+      if (node.data == data) return data
+  
+      if (data < node.data) {
+        node.left = addNode(node.left, data)
+  
+      } else {
+        node.right = addNode(node.right, data)
+      }
+      return node
     }
-    return node
   }
 
   
 
   has(data) {
-    this.rootTree = this.searchNode(this.rootTree, data)
-  }
+    return searchNode(this.rootTree, data)
 
-  searchNode(node, data) {
-    if (!node) return false
-    if (node.data == data) return true
-
-    if (data < node.data) {
-      return this.searchNode(node.left, data)
-    } else {
-      return this.searchNode(node.right, data)
+    function searchNode(node, data) {
+      if (!node) return false
+      if (node.data == data) return true
+  
+      if (data < node.data) {
+        return searchNode(node.left, data)
+      } else {
+        return searchNode(node.right, data)
+      }
     }
   }
+
+  
 
   find(data) {
-    this.rootTree = this.returnNode(this.rootTree, data)
-  }
-
-  returnNode(node ,data) {
-    if (!node) return null
-    if (node.data == data) return node.data
-
-    if (data < node.data) {
-      return this.returnNode(node.left, data)
-    } else {
-      return this.returnNode(node.right, data)
+    return returnNode(this.rootTree, data)
+      
+    function returnNode(node, data) {
+      if (!node) return null
+      if (node.data == data) return node
+  
+      if (data < node.data) {
+        return returnNode(node.left, data)
+      } else {
+        return returnNode(node.right, data)
+      }
     }
   }
+
+  
 
   remove(data) {
-    this.rootTree = this.removeNode(this.rootTree, data)
-  }
+    this.rootTree = removeNode(this.rootTree, data)
 
-  removeNode(node, data) {
-    if (!node) return null
-
-    if (data < node.data) {
-      node.left = this.removeNode(node.left, data)
-      return node
-    } else if (data > node.data) {
-      node.right = this.removeNode(node.right, data)
-      return node
-    } else {
-      if (!node.left && !node.right) return null
-
-      if (!node.left) return node.right
-      if (!node.right) return node.left
-
-      let minFromRight = node.right;
-
-      while (minFromRight.left) {
-        minFromRight = minFromRight.left
+    function removeNode(node, data) {
+      if (!node) return null
+  
+      if (data < node.data) {
+        node.left = removeNode(node.left, data)
+        return node
+      } else if (data > node.data) {
+        node.right = removeNode(node.right, data)
+        return node
+      } else {
+        if (!node.left && !node.right) return null
+  
+        if (!node.left) return node.right
+        if (!node.right) return node.left
+  
+        let minFromRight = node.right;
+  
+        while (minFromRight.left) {
+          minFromRight = minFromRight.left
+        }
+  
+        node.data = minFromRight.data;
+  
+        node.right = removeNode(node.right, minFromRight.data);
+  
+        return node;
       }
-
-      node.data = minFromRight.data;
-
-      node.right = this.removeNode(node.right, minFromRight.data);
-
-      return node;
     }
   }
+
+  
 
   min() {
     if (!this.rootTree) return null
@@ -120,3 +126,15 @@ class BinarySearchTree {
 module.exports = {
   BinarySearchTree
 };
+
+
+const tree = new BinarySearchTree();
+tree.add(2);
+tree.add(7);
+tree.add(1);
+tree.add(8);
+tree.add(4);
+tree.add(32);
+tree.add(12);
+tree.add(14);
+console.log(tree.find(8))//, 8);
